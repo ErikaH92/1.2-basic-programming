3c7dafa52c942747efa53e1bfb951cb832da18f6
Erika <Erika@LAPTOP-2EQ5SS9S>
2024-10-07 21:18:02 -0400

    The nunber of customers will tell us the approximation of people who chose a flavored energy drink

--- student grade.cpp
+++ student grade.cpp
@@ -3,8 +3,27 @@
 
 #include <iostream>
 
+using namespace std;
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+    int number_of_customer
+    double energy;
+    int energy_taken;
+
+    double citrus_flavor=0.0;
+
+
+
+    cout << "enter the number of customers who surveyed at the soft drink company questionare\n";
+
+    cin >> number_of_customer;
+
+  cout << "How many customers drink this amount of energy drink per week?\n";
+
+  
+  return 0;
+
 }
 
