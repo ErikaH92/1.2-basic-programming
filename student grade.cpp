7b748413080ef5f10665fedbb3f3e64765cf364b
Erika <Erika@LAPTOP-2EQ5SS9S>
2024-10-07 23:18:14 -0400

    it shows the percentage of how many people who consumed more than one energy drink prefers a flavored kind

--- student grade.cpp
+++ student grade.cpp
@@ -4,26 +4,42 @@
 #include <iostream>
 
 using namespace std;
+
 int main()
 {
-
-
-    int number_of_customer
-    double energy;
-    int energy_taken;
-
-    double citrus_flavor=0.0;
-
-
-
-    cout << "enter the number of customers who surveyed at the soft drink company questionare\n";
-
-    cin >> number_of_customer;
-
-    cout << "Enter number of customer that consumes more than one or more energy drinks per week.\n";
-
-    cin >> energy_taken;
   
+
+    int number_of_customer = 0;
+    int energy_taken = 0;
+    double citrus_preference = 0.0;
+
+    
+
+        cout << "enter the number of customers who surveyed at the soft drink company questionare\n";
+
+        cin >> number_of_customer;
+    
+        if (number_of_customer > 0) {
+            cout << "Out of the number of customers who surveyed how many of them drink one or more energy drink?\n";
+ cin >> energy_taken;
+        }
+
+
+        if (energy_taken <= number_of_customer) {
+
+            double citrus_preference = (energy_taken * 0.15);
+
+            cout << " out of " << energy_taken << " energy drinks taken " << citrus_preference << " prefer the flavored energy citrus drink. \n";
+       
+
+        }
+
+        else {
+            cout << " energy taken cannot exceed the number of customers. \n";
+        }
+    else {
+        cout << " number of customers must be greater than zero. \n";
+    }
   return 0;
 
 }
